29d78b480a9412cf766b9cd95e5aea8bf50af7e5
Xamek <elzamek@hotmail.com>
2020-11-21 12:00:39 +0200

    should get correct location when moved forward test passed

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -1,13 +1,49 @@
 import { Location } from "./Location";
+import { Directions } from "./constants/Directions";
 
 export class MarsRover {
-    currentlocation!: Location;
 
+    currentlocation!: Location;
+    commands!: Map<string, Function>;
+
+    constructor() {
+        this.loadCommands();
+    }
+
+    loadCommands() {
+        this.commands = new Map<string, Function>();
+        this.commands.set("F", () => { this.moveForward() });
+    }
+
+    moveForward() {
+        const moveForwardMap = {
+            [Directions.NORTH]: () => {
+                this.currentlocation.point.y++;
+            },
+            [Directions.SOUTH]: () => {
+                this.currentlocation.point.y--;
+            },
+            [Directions.EAST]: () => {
+                this.currentlocation.point.x++;
+            },
+            [Directions.WEST]: () => {
+                this.currentlocation.point.x--;
+            },
+        }
+        moveForwardMap[this.currentlocation.direction]();
+    }
 
     deploy(location: Location) {
         this.currentlocation = location;
     }
     getCurrentLocation() {
         return this.currentlocation;
+    }
+
+    executeCommands(commandString: string) {
+        [...commandString].forEach((key: string) => {
+            const command = this.commands.get(key);
+            command != undefined ? command() : null;
+        });
     }
 }
