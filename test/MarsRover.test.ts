fa1e71a00b31bd60e1c7b1bd521f55cd5f34464a
Xamek <elzamek@hotmail.com>
2020-11-21 08:58:14 +0200

    should get correct location when deployed test failed

---
+++ test/MarsRover.test.ts
@@ -0,0 +1,7 @@
+test("should get correct location when deployed", () => {
+    const startLocation = { x: 0, y: 0 };
+    const marsRover = new MarsRover();
+    const location = new Location(startLocation, Directions.NORTH);
+    marsRover.deploy(location);
+    expect(marsRover.getCurrentLocation().toString()).toBe("(0,0) NORTH");
+})
