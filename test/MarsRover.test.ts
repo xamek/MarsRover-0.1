5f425fade1dee6436716c04602d9897659b2511d
Xamek <elzamek@hotmail.com>
2020-11-21 10:28:18 +0200

    should get correct location when moved forward test failed

--- test/MarsRover.test.ts
+++ test/MarsRover.test.ts
@@ -9,3 +9,11 @@ test("should get correct location when deployed", () => {
     marsRover.deploy(location);
     expect(marsRover.getCurrentLocation().toString()).toBe("(0,0) NORTH");
 })
+test("should get correct location when moved forward", () => {
+    const startLocation = { x: 0, y: 0 };
+    const marsRover = new MarsRover();
+    const location = new Location(startLocation, Directions.NORTH);
+    marsRover.deploy(location);
+    marsRover.executeCommands("F");
+    expect(marsRover.getCurrentLocation().toString()).toBe("(0,1) NORTH");
+})
