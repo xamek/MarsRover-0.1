77dd5c5223f6f4ae18aaaa1ef325a130ce10d6f3
Xamek <elzamek@hotmail.com>
2020-11-21 12:16:02 +0200

    refactor

--- test/MarsRover.test.ts
+++ test/MarsRover.test.ts
@@ -1,19 +1,46 @@
 import { Directions } from "../src/constants/Directions";
 import { Location } from "../src/Location";
 import { MarsRover } from "../src/MarsRover";
+import { Point } from "../src/Point";
 
 test("should get correct location when deployed", () => {
-    const startLocation = { x: 0, y: 0 };
+    const startLocation = new Point(0, 0);
     const marsRover = new MarsRover();
     const location = new Location(startLocation, Directions.NORTH);
     marsRover.deploy(location);
     expect(marsRover.getCurrentLocation().toString()).toBe("(0,0) NORTH");
 })
-test("should get correct location when moved forward", () => {
-    const startLocation = { x: 0, y: 0 };
+test("should get correct location when moved forward facing north", () => {
+    const startLocation = new Point(0, 0);
     const marsRover = new MarsRover();
     const location = new Location(startLocation, Directions.NORTH);
     marsRover.deploy(location);
     marsRover.executeCommands("F");
     expect(marsRover.getCurrentLocation().toString()).toBe("(0,1) NORTH");
+})
+test("should get correct location when moved forward facing south", () => {
+    const startLocation = new Point(0, 0);
+    const marsRover = new MarsRover();
+    const location = new Location(startLocation, Directions.SOUTH);
+    marsRover.deploy(location);
+    marsRover.executeCommands("F");
+    expect(marsRover.getCurrentLocation().toString()).toBe("(0,-1) SOUTH");
+})
+
+test("should get correct location when moved forward facing east", () => {
+    const startLocation = new Point(0, 0);
+    const marsRover = new MarsRover();
+    const location = new Location(startLocation, Directions.EAST);
+    marsRover.deploy(location);
+    marsRover.executeCommands("F");
+    expect(marsRover.getCurrentLocation().toString()).toBe("(1,0) EAST");
+})
+
+test("should get correct location when moved forward facing west", () => {
+    const startLocation = new Point(0, 0);
+    const marsRover = new MarsRover();
+    const location = new Location(startLocation, Directions.WEST);
+    marsRover.deploy(location);
+    marsRover.executeCommands("F");
+    expect(marsRover.getCurrentLocation().toString()).toBe("(-1,0) WEST");
 })
